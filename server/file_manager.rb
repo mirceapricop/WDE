05ec89af84be5bd5ad937ce0bea6666ebbe16e69
Mircea Pricop <mircea.a.pricop@gmail.com>
2011-06-11 12:59:23 -0600

    Added storing of a virtual file which mirrors changes done by clients.
    All we need for writing now is to dump this to disk.

--- server/file_manager.rb
+++ server/file_manager.rb
@@ -1,4 +1,5 @@
 require 'singleton'
+require 'json'
 require File.expand_path('virtual_file', File.dirname(__FILE__))
 
 class FileManager
@@ -31,29 +32,27 @@ class FileManager
       if @virtual_files[norm_path].nil?
         load_file(file_path, norm_path)
       end
-      send_file(norm_path, sock)
-
-      # If we got here IO was ok, time to do bookkeeping
-      # First unsubscribe from old file
-      close_down(sock)
-
-      # Subscribe to the new one
-      @socket_file[sock] = norm_path
-      @file_sockets[norm_path] = Array.new unless @file_sockets.has_key? norm_path
-      @file_sockets[norm_path] << sock
-
-      sock.sendClient("FETCH_DONE:", sock.aesKey);
     rescue Exception => e
-      puts e.message
       unless @file_sockets[norm_path].nil?
         @file_sockets[norm_path].delete(sock)
       end
       sock.sendClient("FETCH_FAIL:", sock.aesKey);
+      return;
     end
+    # If we got here IO was ok, time to do bookkeeping
+    send_file(norm_path, sock)
+    # First unsubscribe from old file
+    close_down(sock)
+    # Subscribe to the new one
+    @socket_file[sock] = norm_path
+    @file_sockets[norm_path] = Array.new unless @file_sockets.has_key? norm_path
+    @file_sockets[norm_path] << sock
+    sock.sendClient("FETCH_DONE:", sock.aesKey);
   end
 
   def change(delta, sock)
     file = @socket_file[sock]
+    @virtual_files[file].apply_delta(JSON.parse(delta))
     @file_sockets[file].each do |s|
       if s != sock
         s.sendClient("FETCH_CHANGE:"+delta, s.aesKey)
@@ -62,8 +61,12 @@ class FileManager
   end
 
   def close_down(sock)
-    if @socket_file[sock]
-      @file_sockets[@socket_file[sock]].delete(sock)
+    f = @socket_file[sock]
+    if f
+      @file_sockets[f].delete(sock)
+      if @file_sockets[f].size == 0
+        @virtual_files.delete(f)
+      end
       @socket_file.delete(sock)
     end
   end
--- server/virtual_file.rb
+++ server/virtual_file.rb
@@ -14,4 +14,74 @@ class VirtualFile
     end
   end
 
+  def apply_delta(delta)
+    case delta["action"]
+    when "insertLines"
+      insertLines(delta["range"]["start"]["row"], delta["lines"])
+    when "insertText"
+      insert(delta["range"]["start"], delta["text"])
+    when "removeLines"
+      removeLines(delta["range"]["start"]["row"], delta["range"]["end"]["row"])
+    when "removeText"
+      remove(delta["range"])
+    end
+  end
+
+  def insertLines(row, lines)
+    return if lines.length == 0
+    lines.each do |l|
+      @lines.insert(row, l + "\n")
+      row += 1
+    end
+  end
+
+  def insert(pos, text)
+    return if text.length == 0
+    cur_line = pos["row"]
+    cur_col = pos["column"]
+    while @lines.length-1 < pos["row"]
+      @lines <<  ""
+    end
+    text.each_char do |c|
+      if c == "\n"
+        # This got pretty messy, but there are a lot of corner cases
+        rest = @lines[cur_line][cur_col..-1]
+        @lines.insert(cur_line+1, rest)
+        if cur_col > 0
+          @lines[cur_line] = @lines[cur_line][0..cur_col-1]+"\n"
+        else
+          @lines[cur_line] = "\n"
+        end
+        cur_line += 1
+        cur_col = 0
+      else
+        @lines[cur_line].insert(cur_col, c)
+        cur_col += 1
+      end
+    end
+  end
+
+  def removeLines(firstRow, lastRow)
+    @lines = @lines[0..firstRow][0..-2] + @lines[lastRow..-1]
+  end
+
+  def remove(range)
+    firstRow = range["start"]["row"]
+    lastRow = range["end"]["row"]
+    num_rows = lastRow - firstRow
+    if num_rows >= 1
+      lr = @lines[lastRow]
+      @lines[lastRow] = lr[range["end"]["column"]..-1]
+      removeLines(firstRow+1, lastRow-1) if num_rows >= 2
+      @lines[firstRow] = @lines[firstRow][0..range["start"]["column"]][0..-2]
+      # Merge lines if \n was deleted
+      if @lines[firstRow][-1] != "\n"
+        @lines[firstRow] += @lines[lastRow]
+        @lines.delete_at(lastRow)
+      end
+    else
+      fr = @lines[firstRow]
+      @lines[firstRow]=fr[0..range["start"]["column"]][0..-2]+fr[range["end"]["column"]..-1]
+    end
+  end
 end
